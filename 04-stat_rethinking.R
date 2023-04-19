07d267836771b005a86c87f925121a0f283114a3
Heather Talbott <hatalbott2@gmail.com>
2023-04-19 16:32:38 -0600

    continue book chapter 4

--- 04-stat_rethinking.R
+++ 04-stat_rethinking.R
@@ -118,6 +118,296 @@ dens(sample2.sigma, norm.comp = TRUE)
 
 ## 4.3.5 Find posterior distribution -------------------------------------------------------------------
 
+flist <- alist(
+  height ~ dnorm(mu, sigma)
+  , mu ~ dnorm(178, 20)
+  , sigma ~ dunif(0, 50)
+)
+
+m4.1 <- quap(flist, data = d2)
+
+precis(m4.1)
+
+flist <- 
+
+m4.2 <- quap(alist(
+  height ~ dnorm(mu, sigma)
+  , mu ~ dnorm(178, 0.1)
+  , sigma ~ dunif(0, 50)
+)
+, data = d2
+)
+
+precis(m4.2)
+
+vcov(m4.1)
+
+diag(vcov(m4.1))
+
+cov2cor(vcov(m4.1))
+
+post <- extract.samples(m4.1, n = 1e4)
+
+head(post)
+
+precis(post)
+
+plot(post)
+
+
+## 4.4 Linear prediction -------------------------------------------------------
+
+plot(d2$height ~ d2$weight)
+
+
+## 4.4.1 The linear model strategy -----------------------------------------
+
+set.seed(2971)
+
+N <- 100
+
+a <- rnorm(N, 178, 20)
+
+b <- rnorm(N, 0, 10)
+
+plot(NULL
+     , xlim = range(d2$weight)
+     , ylim=c(-100, 400)
+     , xlab="weight"
+     , ylab="height"
+     )
+
+abline(h=0, lty=2)
+
+abline(h=272, lty=1, lwd=0.5)
+
+mtext("b ~ dnorm(0, 10)")
+
+xbar <- 
+
+for (i in 1:N) {
+  curve(a[i] + b[i]*(x-mean(d2$weight))
+        , from=min(d2$weight)
+        , to=max(d2$weight)
+        , add=TRUE
+        , col=col.alpha("black", 0.2)
+        )
+}
+
+c <- rlnorm(1e4, 0, 1)
+
+dens(c)
+
+set.seed(2971)
+
+b2 <- rlnorm(N, 0, 1)
+
+plot(NULL
+     , xlim = range(d2$weight)
+     , ylim=c(-100, 400)
+     , xlab="weight"
+     , ylab="height"
+)
+
+abline(h=0, lty=2)
+
+abline(h=272, lty=1, lwd=0.5)
+
+mtext("log(b2) ~ dnorm(0, 1)")
+
+  for (i in 1:N) {
+    curve(a[i] + b2[i]*(x-mean(d2$weight))
+          , from=min(d2$weight)
+          , to=max(d2$weight)
+          , add=TRUE
+          , col=col.alpha("black", 0.2)
+    )
+  }
+
+
+## 4.4.2 Finding posterior distribution ------------------------------------
+
+m4.3 <- quap(
+  alist(
+    height ~ dnorm(mu, sigma)
+    , mu <- a + b * (weight - mean(d2$weight))
+    , a ~ dnorm(178, 20)
+    , b ~ dlnorm(0, 1)
+    , sigma ~ dunif(0, 50)
+  ), data = d2
+)
+
+precis(m4.3)
+
+round(vcov(m4.3), 3)
+
+plot(height ~ weight, data=d2, col=rangi2)
+
+post <- extract.samples(m4.3)
+
+a_map <- mean(post$a)
+
+b_map <- mean(post$b)
+
+curve(a_map+b_map*(x-mean(d2$weight)), add=TRUE)
+
+head(post)
+
+dN <- d2[1:10,]
+
+mN <- quap(
+  alist(
+    height ~ dnorm(mu, sigma)
+    , mu <- a + b * (weight - mean(weight))
+    , a ~ dnorm(178, 20)
+    , b ~ dlnorm(0, 1)
+    , sigma ~ dunif(0, 50)
+  ), data = dN
+)
+
+#extract 20 samples from posterior
+post <- extract.samples(mN, n=20)
+
+#display raw data and sample size
+plot(dN$weight
+    , dN$height
+    , xlim=range(d2$weight)
+    , ylim=range(d2$height)
+    , col=rangi2
+    , xlab="weight"
+    , ylab="height"
+    )
+
+mtext(concat("N = ", nrow(dN)))
+
+#plot the lines, with transparency
+for(i in 1:nrow(post)){
+curve(post$a[i] + post$b[i]*(x-mean(dN$weight))
+      , col=col.alpha("black", 0.3)
+      , add = TRUE
+      )
+}
+
+dN <- d2[1:300,]
+
+mN <- quap(
+  alist(
+    height ~ dnorm(mu, sigma)
+    , mu <- a + b * (weight - mean(weight))
+    , a ~ dnorm(178, 20)
+    , b ~ dlnorm(0, 1)
+    , sigma ~ dunif(0, 50)
+  ), data = dN
+)
+
+#extract 20 samples from posterior
+post <- extract.samples(mN, n=20)
+
+#display raw data and sample size
+plot(dN$weight
+     , dN$height
+     , xlim=range(d2$weight)
+     , ylim=range(d2$height)
+     , col=rangi2
+     , xlab="weight"
+     , ylab="height"
+)
+
+mtext(concat("N = ", nrow(dN)))
+
+#plot the lines, with transparency
+for(i in 1:nrow(post)){
+  curve(post$a[i] + post$b[i]*(x-mean(dN$weight))
+        , col=col.alpha("black", 0.3)
+        , add = TRUE
+  )
+}
+
+post <- extract.samples(m4.3)
+
+mu_at_50 <- post$a + post$b * (50-mean(d2$weight))
+
+dens(mu_at_50)
+
+PI(mu_at_50, prob=0.89)
+
+mu <- link(m4.3)
+
+str(mu)
+
+weight.seq <- seq(from=25, to=70, by=1)
+
+mu_sp <- link(m4.3, data=data.frame(weight=weight.seq))
+
+str(mu_sp)
+
+plot(height ~ weight, d2)
+
+for (i in 1:1000) (
+  points(weight.seq
+         , mu_sp[i,]
+         , pch=16
+         , col=col.alpha(rangi2, 0.1)
+         )
+)
+
+mu_sp.mean <- apply(mu_sp, 2, mean)
+mu_sp.PI <- apply(mu_sp, 2, PI, prob=0.89)
+
+#plot raw data
+plot(height ~ weight
+     , data = d2
+     , col=col.alpha(rangi2, 0.5)
+     )
+
+#plot the MAP line aka mean mu
+lines(weight.seq, mu_sp.mean)
+
+#plot a shaded region for 89% PI
+shade(mu_sp.PI, weight.seq)
+
+
+mu.mean <- apply(mu, 2, mean)
+mu.PI <- apply(mu, 2, PI, prob=0.89)
+
+#plot raw data
+plot(height ~ weight
+     , data = d2
+     , col=col.alpha(rangi2, 0.5)
+)
+
+#plot the MAP line aka mean mu
+lines(d2$weight, mu.mean)
+
+#plot a shaded region for 89% PI
+lines(d2$weight, mu.PI["5%",], col="gray")
+lines(d2$weight, mu.PI["94%",], col="gray")
+
+sim.height <- sim(m4.3, data=list(weight=weight.seq))
+str(sim.height)
+
+height.PI <- apply(sim.height, 2, PI, prob=0.89)
+height.PI67 <- apply(sim.height, 2, PI, prob=0.67)
+height.PI89 <- apply(sim.height, 2, PI, prob=0.89)
+height.PI97 <- apply(sim.height, 2, PI, prob=0.97)
+
+#plot raw data
+plot(height ~ weight
+     , data = d2
+     , col=col.alpha(rangi2, 0.5)
+)
+
+#plot the MAP line aka mean mu
+lines(weight.seq, mu_sp.mean)
+
+#plot a shaded region for 89% PI
+shade(mu_sp.PI, weight.seq)
+
+#draw PI region for simulated heights
+shade(height.PI67, weight.seq)
+shade(height.PI89, weight.seq)
+shade(height.PI97, weight.seq)
+
 
 # Video -------------------------------------------------------------------
 
